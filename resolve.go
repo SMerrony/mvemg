75bdfd3984516a6edce498d5b128297e3d3c168d
Stephen Merrony <steve@stephenmerrony.co.uk>
2017-05-28 10:53:28 +0100

    Fix bug in resolve16bitEclipseAddr

--- resolve.go
+++ resolve.go
@@ -8,62 +8,73 @@ import (
 
 func resolve16bitEclipseAddr(cpuPtr *Cpu, ind byte, mode string, disp int32) dg_phys_addr {
 
-	eff := dg_phys_addr(disp) & 0x0ffff
+	var (
+		eff     dg_phys_addr
+		intEff  int32
+		indAddr dg_word
+	)
 
 	// handle addressing mode...
 	switch mode {
 	case "Absolute":
-		// nothing to do
+		intEff = disp
 	case "PC":
-		eff += dg_phys_addr(cpuPtr.pc) & 0x0ffff
+		intEff = int32(cpuPtr.pc) + disp
 	case "AC2":
-		eff += dg_phys_addr(cpuPtr.ac[2]) & 0x0ffff
+		intEff = int32(cpuPtr.ac[2]) + disp
 	case "AC3":
-		eff += dg_phys_addr(cpuPtr.ac[3]) & 0x0ffff
+		intEff = int32(cpuPtr.ac[3]) + disp
 	}
 
 	// handle indirection
 	if ind == '@' { // down the rabbit hole...
-		indAddr := memReadWord(eff)
+		indAddr = memReadWord(dg_phys_addr(intEff))
 		for testWbit(indAddr, 0) {
 			indAddr = memReadWord(dg_phys_addr(indAddr))
 		}
-		eff = dg_phys_addr(indAddr)
+		intEff = int32(indAddr)
 	}
 
 	// mask off to Eclipse range
-	eff &= 0x7fff
+	eff = dg_phys_addr(intEff) & 0x7fff
 
 	debugPrint(SYSTEM_LOG, fmt.Sprintf("... resolve16bitEclipseAddr got: %d., returning %d.\n", disp, eff))
 	return eff
 }
 
+// This is the same as resolve16bitEclipseAddr, but without the range masking at the end
 func resolve16bitEagleAddr(cpuPtr *Cpu, ind byte, mode string, disp int32) dg_phys_addr {
 
-	eff := dg_phys_addr(disp)
+	var (
+		eff     dg_phys_addr
+		intEff  int32
+		indAddr dg_word
+	)
 
 	// handle addressing mode...
 	switch mode {
 	case "Absolute":
-		// nothing to do
+		intEff = disp
 	case "PC":
-		eff += dg_phys_addr(cpuPtr.pc)
+		intEff = int32(cpuPtr.pc) + disp
 	case "AC2":
-		eff += dg_phys_addr(cpuPtr.ac[2])
+		intEff = int32(cpuPtr.ac[2]) + disp
 	case "AC3":
-		eff += dg_phys_addr(cpuPtr.ac[3])
+		intEff = int32(cpuPtr.ac[3]) + disp
 	}
 
 	// handle indirection
 	if ind == '@' { // down the rabbit hole...
-		indAddr := memReadWord(eff)
+		indAddr = memReadWord(dg_phys_addr(intEff))
 		for testWbit(indAddr, 0) {
 			indAddr = memReadWord(dg_phys_addr(indAddr))
 		}
-		eff = dg_phys_addr(indAddr)
+		intEff = int32(indAddr)
 	}
 
-	debugPrint(SYSTEM_LOG, fmt.Sprintf("... resolve16bitEclipseAddr got: %d., returning %d.\n", disp, eff))
+	eff = dg_phys_addr(intEff)
+
+	debugPrint(SYSTEM_LOG, fmt.Sprintf("... resolve16bitEagleAddr got: %d., returning %d.\n", disp, eff))
 	return eff
 }
 
