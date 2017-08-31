39576f83ea6ec016383a1cdc39eb4956c7022867
Steve Merrony <steve@stephenmerrony.co.uk>
2017-08-31 12:03:52 +0200

    Fix erroneous sending of ESC to SCP

--- tti.go
+++ tti.go
@@ -53,10 +53,12 @@ func ttiListener(cpuPtr *CPUT, scpChan chan<- byte) {
 		}
 		//log.Printf("DEBUG: ttiListener() got <%c>\n", b[0])
 		for c := 0; c < n; c++ {
+			// console ESCape?
 			if b[c] == ASCII_ESC || b[c] == 0 {
 				cpuPtr.cpuMu.Lock()
 				cpuPtr.scpIO = true
 				cpuPtr.cpuMu.Unlock()
+				break // don't want to send the ESC itself to the SCP
 			}
 			cpuPtr.cpuMu.RLock()
 			scp := cpuPtr.scpIO
